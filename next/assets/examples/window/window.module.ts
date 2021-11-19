335dba49fac9acae42e800273709ac3a543abeb6
katebatura <katebatura@gmail.com>
2021-11-19 09:55:03 +0300

    Auto-generated commit

---
+++ next/assets/examples/window/window.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { NbButtonModule, NbCardModule, NbCheckboxModule, NbInputModule, NbWindowModule } from '@nebular/theme';
+
+import { WindowRoutingModule } from './window-routing.module';
+import { TemplateWindowComponent } from './template-window.component';
+import { WindowShowcaseComponent } from './window-showcase.component';
+import { WindowsBackdropComponent } from './windows-backdrop.component';
+import { FormComponent } from './components/form.component';
+import { WindowControlsComponent } from './window-controls.component';
+import { WindowTemplateTitleComponent } from './window-template-title.component';
+
+@NgModule({
+  declarations: [
+    TemplateWindowComponent,
+    WindowShowcaseComponent,
+    WindowsBackdropComponent,
+    FormComponent,
+    WindowControlsComponent,
+    WindowTemplateTitleComponent,
+  ],
+  imports: [
+    NbWindowModule.forRoot(),
+    NbButtonModule,
+    NbInputModule,
+    NbCheckboxModule,
+    NbCardModule,
+    WindowRoutingModule,
+  ],
+  entryComponents: [ FormComponent ],
+})
+export class WindowModule {}
